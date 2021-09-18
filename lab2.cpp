5daaf872584777d594bb2d557d256ca7d5d46715
K M Tso <kmtso@live.com>
2021-09-18 15:36:56 +0800

    alpha7

--- lab2.cpp
+++ lab2.cpp
@@ -77,7 +77,7 @@ int main()
 
             if(g1 == p1){
                 redpin++;
-            }else if ((g1==p0 && g0!=p0)||(g1==p2 && g2!=p2)||(g1==p3 && g3!=p3))
+            }else if ((g1==p0 && g0!=p0)||(g1==p2 && g2!=p2 && g0!=p2)||(g1==p3 && g3!=p3 && (g0!=p3 || (g0==p3 && p3==p2))))
             {whitepin++;}
            cout<< "Red pins:"<< redpin << ", White pins:" << whitepin <<endl;
         }
@@ -85,7 +85,7 @@ int main()
 
             if(g2 == p2)
                 redpin++;
-            else if ((g2==p0 && g0!=p0 &&g1!=p0)||(g2==p1 && g1!=p1 && g0!=p1)||(g2==p3 && g3!=p3))
+            else if ((g2==p0 && g0!=p0 &&(g1!=p0 ||(g1==p0 &&p0==p1)))||(g2==p1 && g1!=p1 && (g0!=p1 ||(g0==p3 && p3==p1)))||(g2==p3 && g3!=p3 &&(g1!=p3 ||((g1==p3 && (p1==p3|| p3==p0)))) && (g0!=p3||(g0==p3 && p3==p0))))
                 whitepin++;
          cout<< "Red pins:"<< redpin << ", White pins:" << whitepin <<endl;
         }
@@ -93,17 +93,17 @@ int main()
 
             if(g3 == p3)
                 redpin++;
-            else if ((g3==p0 && g0!=p0 && g1!=p0 && g2!=p0)||(g3==p1 && g1!=p1 && g2!=p1 && g0!=p1)||(g3==p2 && g2!=p2))
+            else if ((g3==p0 && g0!=p0 && g1!=p0 && g2!=p0)||(g3==p1 && g1!=p1 && (g2!=p1 || (g2==p1&&p1==p0)) && (g0!=p1||(g0==p1 && p2==p1)))||(g3==p2 && g2!=p2 && (g1!=p2 ||(g1==p2 &&p2==p0)) && (g0!=p2||(g0==p2 && p2==p0))))
                 whitepin++;
            cout<< "Red pins:"<< redpin << ", White pins:" << whitepin <<endl;
         }
        //             cout<< "Red pins:"<< redpin << ", White pins:" << whitepin <<endl;
 
         if (redpin ==4){
-            cout<< "Code breaker, you have " << attempt <<" attempts to crack his code." << endl;
+            //cout<< "Code breaker, you have " << attempt <<" attempts to crack his code." << endl;
             isGameover=true;
         }else{
-        //    cout<< "Red pins:"<< redpin << ", White pins:" << whitepin <<endl;
+            cout<< "Red pins:"<< redpin << ", White pins:" << whitepin <<endl;
 
             redpin=0;
             whitepin=0;
