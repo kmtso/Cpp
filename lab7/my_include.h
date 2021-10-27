461815b7a8aa6d2db6f6de54be3b66ee01f0d552
K M Tso <kmtso@live.com>
2021-10-27 19:26:35 +0800

    task 1 completed

--- lab7/my_include.h
+++ lab7/my_include.h
@@ -8,6 +8,8 @@ const int MAX_ARRAY_LENGTH = 100;
 /* External function declarations */
 void printArray(const double [], int );
 void printArray(const char [][MAX_ARRAY_LENGTH + 1], int );
+void bubbleSort(double arr[], int n, bool ascending=true);
+void bubbleSort(char arr[][MAX_ARRAY_LENGTH+1], int n, bool ascending=true);
 
 /* Task Three */
 // Add the function declarations here:
--- lab7/utils.cpp
+++ lab7/utils.cpp
@@ -6,15 +6,43 @@
 /* Task One */
 void bubbleSort(double arr[], int n, bool ascending)
 {
-   
+       bool swapped=false;
+       bool swapped2=false;
+       int arrLen=n;
+       double temp;
+       if (ascending){
+         do{
+            swapped=false;
+            for(int i=0;i< arrLen-1;i++){
+               if(arr[i]>arr[i+1]){
+                  temp=arr[i];
+                  arr[i]=arr[i+1];
+                  arr[i+1]=temp;
+                  swapped=true;
+            }
+        
+            }}while (swapped);
+      }else{
+                  do{
+            swapped2=false;
+            for(int i=0;i< arrLen-1;i++){
+               if(arr[i]<arr[i+1]){
+                  temp=arr[i];
+                  arr[i]=arr[i+1];
+                  arr[i+1]=temp;
+                  swapped2=true;
+            }
+        
+            }}while (swapped2);
+      }
 }
 
-
+//g++ -o lab7.exe main.cpp utils.cpp
 
 /* Task Two */
 void bubbleSort(char arr[][MAX_ARRAY_LENGTH+1], int n, bool ascending)
 {
-   
+
 }
 
 
