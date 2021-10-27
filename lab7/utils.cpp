023fd0ca85d503a8200a4daf9fb5244886ae8e4d
K M Tso <kmtso@live.com>
2021-10-27 18:22:42 +0800

    lab7 skeleton codes

---
+++ lab7/utils.cpp
@@ -0,0 +1,36 @@
+#include <iostream> /* COMP2011 Lab 7: utils.cpp */
+#include <cstring>
+#include "my_include.h"
+
+
+/* Task One */
+void bubbleSort(double arr[], int n, bool ascending)
+{
+   
+}
+
+
+
+/* Task Two */
+void bubbleSort(char arr[][MAX_ARRAY_LENGTH+1], int n, bool ascending)
+{
+   
+}
+
+
+
+/* Given Functions */
+void printArray(const double arr[], int size)
+{
+   for (int i = 0; i < size; i++)
+      cout << arr[i] << " ";
+   cout << endl;
+}
+
+void printArray(const char arr[][MAX_ARRAY_LENGTH+1], int size)
+{
+   for (int i = 0; i < size; i++)
+      cout << arr[i] << endl;
+   cout << endl;
+}
+
